e011d58c6717359d940c2dca6784749f8a29a0af
Lxler <10110553+lxlstudyprogram@user.noreply.gitee.com>
2024-06-04 15:32:19 +0800

    fix(seatmng): fix the bug of seatcount

--- myg6-cosmic-debug/src/main/java/plugins/seatmng/AddSeatCount.java
+++ myg6-cosmic-debug/src/main/java/plugins/seatmng/AddSeatCount.java
@@ -1,18 +1,15 @@
 package plugins.seatmng;
 
+
 import kd.bos.dataentity.entity.DynamicObject;
-import kd.bos.db.DB;
-import kd.bos.db.DBRoute;
-import kd.bos.entity.EntityMetadataCache;
-import kd.bos.entity.operate.Operations;
+import kd.bos.entity.ExtendedDataEntity;
 import kd.bos.entity.plugin.AbstractOperationServicePlugIn;
 import kd.bos.entity.plugin.PreparePropertysEventArgs;
-import kd.bos.orm.query.QCP;
-import kd.bos.orm.query.QFilter;
-import kd.bos.servicehelper.BusinessDataServiceHelper;
 import kd.bos.servicehelper.operation.SaveServiceHelper;
 import kd.sdk.plugin.Plugin;
 import kd.bos.entity.plugin.args.AfterOperationArgs;
+
+import java.util.List;
 
 /**
  * 单据操作插件
@@ -21,39 +18,26 @@ public class AddSeatCount extends AbstractOperationServicePlugIn implements Plug
 
     private final static String SEAT = "tk_myg6_seat_seats";
 
-    private final static String ROOM = "myg6_room";
+    private final static String ROOM = "myg6_basedatafield";
 
     private final static String ROOM_ID = "number";
 
     @Override
     public void onPreparePropertys(PreparePropertysEventArgs e) {
         //加载自习室id字段
-        e.getFieldKeys().add(ROOM_ID);
+        e.getFieldKeys().add(ROOM);
     }
 
     @Override
     public void afterExecuteOperationTransaction(AfterOperationArgs e) {
         //座位表
-        //获取DynamicObject列表
-        String fields = "myg6_basedatafield";
-        // Create an empty filter array (no filters)
-        QFilter[] filters = new QFilter[0];
-        // Load the data
-        DynamicObject entity = BusinessDataServiceHelper.loadSingle("myg6_seat", fields, filters);
-        System.out.println("fuckinfo: " + entity);
-        while (true) {
-            DynamicObject o = (DynamicObject)entity.get("myg6_basedatafield");
-            System.out.println("fuck" + o);
-            if (o == null) {
-                continue;
-            }
+        List<ExtendedDataEntity> rows = e.getSelectedRows();
+        for (ExtendedDataEntity row : rows) {
+            DynamicObject dataEntity = row.getDataEntity();
+            DynamicObject o = (DynamicObject)dataEntity.get("myg6_basedatafield");
             Integer num = Integer.valueOf(o.getString("myg6_integerfield1")) + 1;
-            System.out.println("fuck" + num);
             o.set("myg6_integerfield1", num.toString());
             SaveServiceHelper.update(o);
-            if (o != null) {
-                break;
-            }
         }
     }
 }
