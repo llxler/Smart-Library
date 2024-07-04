9d91866832d1abddafb6cd463849e58735517f08
Lxler <10110553+lxlstudyprogram@user.noreply.gitee.com>
2024-07-04 22:36:20 +0800

    fix echarts

--- myg6-cosmic-debug/src/main/java/plugins/extendedfun/QAanal.java
+++ myg6-cosmic-debug/src/main/java/plugins/extendedfun/QAanal.java
@@ -5,6 +5,7 @@ import kd.bos.entity.IFrameMessage;
 import kd.bos.form.FormShowParameter;
 import kd.bos.form.container.Container;
 import kd.bos.form.control.IFrame;
+import kd.bos.form.events.ClientCallBackEvent;
 import kd.bos.form.plugin.AbstractFormPlugin;
 import kd.sdk.plugin.Plugin;
 
@@ -19,33 +20,19 @@ public class QAanal extends AbstractFormPlugin implements Plugin {
         super.afterBindData(e);
         IFrame iframe = this.getControl("myg6_frame_echarts");
         iframe.setSrc("http://localhost:12348/");
-    }
-
-    @Override
-    public void registerListener(EventObject e) {
-        super.registerListener(e);
-        //添加按钮监听
-        Container flex = this.getView().getControl("myg6_flexpanelap");
-        flex.addClickListener(this);
+        this.getView().addClientCallBack("10", 200);
     }
     @Override
-    public void click(EventObject evt) {
-        super.click(evt);
-        Object source = evt.getSource();
-        if (source instanceof Container) {
-            Container button = (Container) source;
-            String key = button.getKey();
-            if (StringUtils.equals("myg6_flexpanelap", key)) {
-                FormShowParameter showParameter = this.getView().getFormShowParameter();
-                String data = showParameter.getCustomParam("qaResult");
-                System.out.println("now data: " + data);
-                IFrame iframe = this.getControl("myg6_frame_echarts");
-                IFrameMessage message = new IFrameMessage();
-                message.setType("qabot");
-                message.setOrigin("*");
-                message.setContent(data);
-                iframe.postMessage(message);
-            }
-        }
+    public void clientCallBack(ClientCallBackEvent e) {
+        super.clientCallBack(e);
+        FormShowParameter showParameter = this.getView().getFormShowParameter();
+        String data = showParameter.getCustomParam("qaResult");
+        System.out.println("now data: " + data);
+        IFrame iframe = this.getControl("myg6_frame_echarts");
+        IFrameMessage message = new IFrameMessage();
+        message.setType("qabot");
+        message.setOrigin("*");
+        message.setContent(data);
+        iframe.postMessage(message);
     }
 }
---
+++ myg6-cosmic-debug/src/main/java/plugins/homepage/ShowEchartHomepg.java
@@ -0,0 +1,31 @@
+package plugins.homepage;
+
+import kd.bos.entity.IFrameMessage;
+import kd.bos.form.control.IFrame;
+import kd.bos.form.events.ClientCallBackEvent;
+import kd.bos.form.plugin.AbstractFormPlugin;
+import kd.sdk.plugin.Plugin;
+
+import java.util.EventObject;
+
+/**
+ * 动态表单插件
+ */
+public class ShowEchartHomepg extends AbstractFormPlugin implements Plugin {
+    @Override
+    public void afterBindData(EventObject e) {
+        super.afterBindData(e);
+        IFrame iframe = this.getControl("myg6_frame_echarts");
+        iframe.setSrc("http://localhost:12348/");
+        this.getView().addClientCallBack("10", 1500);
+    }
+    @Override
+    public void clientCallBack(ClientCallBackEvent e) {
+        super.clientCallBack(e);
+        IFrame iframe = this.getControl("myg6_frame_echarts");
+        IFrameMessage message = new IFrameMessage();
+        message.setType("home");
+        message.setOrigin("*");
+        iframe.postMessage(message);
+    }
+}
